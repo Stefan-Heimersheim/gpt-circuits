f7f9a59e379eb605fc5388adbdd8759afa4aead3
David Quarel <davidquarel@gmail.com>
2025-05-20 16:31:48 +0000

    moved training files for SAEs

---
+++ run/jblock_E6.sh
@@ -0,0 +1,43 @@
+# E6 series: 1.0, 1.5, 2.2, 3.3, 4.7, 6.8
+
+python -m training.sae.jsae_block --sparsity=1.0e-1
+python -m training.sae.jsae_block --sparsity=1.5e-1
+python -m training.sae.jsae_block --sparsity=2.2e-1
+python -m training.sae.jsae_block --sparsity=3.3e-1
+python -m training.sae.jsae_block --sparsity=4.7e-1
+python -m training.sae.jsae_block --sparsity=6.8e-1
+
+python -m training.sae.jsae_block --sparsity=1.0e-2
+python -m training.sae.jsae_block --sparsity=1.5e-2
+python -m training.sae.jsae_block --sparsity=2.2e-2
+python -m training.sae.jsae_block --sparsity=3.3e-2
+python -m training.sae.jsae_block --sparsity=4.7e-2
+python -m training.sae.jsae_block --sparsity=6.8e-2
+
+python -m training.sae.jsae_block --sparsity=1.0e-3
+python -m training.sae.jsae_block --sparsity=1.5e-3
+python -m training.sae.jsae_block --sparsity=2.2e-3
+python -m training.sae.jsae_block --sparsity=3.3e-3
+python -m training.sae.jsae_block --sparsity=4.7e-3
+python -m training.sae.jsae_block --sparsity=6.8e-3
+
+python -m training.sae.jsae_block --sparsity=1.0e-4
+python -m training.sae.jsae_block --sparsity=1.5e-4
+python -m training.sae.jsae_block --sparsity=2.2e-4
+python -m training.sae.jsae_block --sparsity=3.3e-4
+python -m training.sae.jsae_block --sparsity=4.7e-4
+python -m training.sae.jsae_block --sparsity=6.8e-4
+
+python -m training.sae.jsae_block --sparsity=1.0e-5
+python -m training.sae.jsae_block --sparsity=1.5e-5
+python -m training.sae.jsae_block --sparsity=2.2e-5
+python -m training.sae.jsae_block --sparsity=3.3e-5
+python -m training.sae.jsae_block --sparsity=4.7e-5
+python -m training.sae.jsae_block --sparsity=6.8e-5
+
+python -m training.sae.jsae_block --sparsity=1.0e-6
+python -m training.sae.jsae_block --sparsity=1.5e-6
+python -m training.sae.jsae_block --sparsity=2.2e-6
+python -m training.sae.jsae_block --sparsity=3.3e-6
+python -m training.sae.jsae_block --sparsity=4.7e-6
+python -m training.sae.jsae_block --sparsity=6.8e-6
